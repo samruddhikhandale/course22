0a6b785520620df56b2b0537acea370795dc8a41
Samruddhi Khandale <skhandale@microsoft.com>
2023-02-03 12:11:50 -0800

    Delete install.sh

--- tools/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-echo "(*) Installing tools..."
-
-python3 -m pip install -r requirements.txt
-
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends graphviz
